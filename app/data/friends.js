a120920693ffe0d8d1e36fdccc4d11772bf20601
Briana Oursler <boursler@blackhole.localdomain>
2018-04-28 09:03:50 -0700

    More form validation, plus initial values in friends.js as well as a comment for friend-finding logic

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+	{
+		"name": "Ahmed",
+		"photo": "https://pixabay.com/en/water-waterfall-motion-outdoors-3354062/",
+		"scores": [
+			5,
+			1,
+			4,
+			4,
+			5,
+			1,
+			2,
+			5,
+			4,
+			1
+		]
+	},
+	{
+		"name": "Danielle",
+		"photo": "https://pixabay.com/en/woman-girl-curler-hairstyle-3170568/",
+		"scores": [
+			4,
+			1,
+			4,
+			5,
+			2,
+			1,
+			3,
+			5,
+			3,
+			1,
+		]
+	}
+]
+module.exports = friends;
